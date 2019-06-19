76b20c629a6fd95741ee73d2982e4522a8875031
duveigr <gregory.duveiller@ec.europa.eu>
2019-06-19 10:00:53 +0000

    Edit first version of combined plot

--- codeFigures/make_figures4albedo-covercrop-paper.r
+++ codeFigures/make_figures4albedo-covercrop-paper.r
@@ -6,6 +6,9 @@ library(RColorBrewer)
 library(here)
 
 
+fpath <- 'textFigures/'
+
+
 # load data
 load(file = 'dataFigures/redux_data4fig.Rda') # 'GHGbdg','GHGsen', 'aLCS'
 
@@ -25,17 +28,71 @@ pts_sf <- aLCS %>%
   st_set_crs(st_crs(world)) %>%
   st_transform(laes_prj)
 
-g.map.delta <- ggplot(pts_sf) + 
+
+
+custom_theme <- theme(legend.position = 'top',
+                      legend.key.width = unit(0.8,'in'))
+
+pointSize <- 0.2
+
+g.map.TransSWin <- ggplot(pts_sf) + 
   geom_sf(data = europe_laea, fill = 'grey50')+
-  geom_sf(aes(colour = SWin_Ta_Wm2))+
-  scale_colour_gradientn(limits = c(40,120), 
+  geom_sf(aes(colour = SWin_Ta_Wm2), size = pointSize)+
+  scale_colour_gradientn("Ta * SWin",
+                         limits = c(40,120), 
                          colors = rev(brewer.pal(9,'Spectral')), oob = squish)+
+  coord_sf(xlim = c(2.5e6,6e6), ylim = c(1.5e6,4.5e6))+
+  custom_theme +
+  guides(colour = guide_colourbar(title.position = 'top', title.hjust = 0.5))
+
+
+g.map.BareSoilA <- ggplot(pts_sf) + 
+  geom_sf(data = europe_laea, fill = 'grey50')+
+  geom_sf(aes(colour = BareSoil.Albedo, size = pointSize))+
+  scale_colour_viridis_c("Bare soil albedo",
+                         limits = c(0.03,0.32), 
+                         option = "viridis", oob = squish)+
+  coord_sf(xlim = c(2.5e6,6e6), ylim = c(1.5e6,4.5e6))+
+  custom_theme +
+  guides(colour = guide_colourbar(title.position = 'top', title.hjust = 0.5))
+
+
+g.map.AlbedoChg <- ggplot(pts_sf) + 
+  geom_sf(data = europe_laea, fill = 'grey50')+
+  geom_sf(aes(colour = a_dif, size = pointSize))+
+  scale_colour_gradientn("Albedo change",
+                         limits = c(-0.03,0.03), 
+                         colors = rev(brewer.pal(9,'RdBu')), oob = squish)+
+  coord_sf(xlim = c(2.5e6,6e6), ylim = c(1.5e6,4.5e6))+
+  custom_theme +
+  guides(colour = guide_colourbar(title.position = 'top', title.hjust = 0.5))
+
+
+g.map.AlbRadFor <- ggplot(pts_sf) + 
+  geom_sf(data = europe_laea, fill = 'grey50')+
+  geom_sf(aes(colour = RFa_Wm2, size = pointSize))+
+  scale_colour_gradientn("Albedo radiative forcing",
+                         limits = c(-1.5,1.5), 
+                         colors = rev(brewer.pal(9,'RdBu')), oob = squish)+
   coord_sf(xlim = c(2.5e6,6e6), ylim = c(1.5e6,4.5e6))+
   theme(legend.position = 'bottom',
         legend.key.width = unit(1,'in'))+
   guides(colour = guide_colourbar(title.position = 'top', title.hjust = 0.5))
 
-ggsave(filename = paste0('DeltaCC_',iYear,'_LbMv','.png'), plot = g.map.delta, path = fpath, width = 12, height = 10)
+
+fname <- 'Figure1'
+figW <- 12; figH <- 10; fmt <- 'png'
+fullfname <- paste0(fpath, fname, '.', fmt)
+if(fmt=='png'){png(fullfname, width = figW, height = figH, units = "in", res= 150)}
+if(fmt=='pdf'){pdf(fullfname, width = figW, height = figH)}
+print(g.map.TransSWin, vp = viewport(width = 0.5, height = 0.5, x = 0.0, y = 0.5, just=c(0,0)))
+print(g.map.BareSoilA, vp = viewport(width = 0.5, height = 0.5, x = 0.5, y = 0.5, just=c(0,0)))
+print(g.map.AlbedoChg, vp = viewport(width = 0.5, height = 0.5, x = 0.0, y = 0.0, just=c(0,0)))
+print(g.map.AlbRadFor, vp = viewport(width = 0.5, height = 0.5, x = 0.5, y = 0.0, just=c(0,0)))
+dev.off()
+
+
+#ggsave(filename = paste0('DeltaCC_',iYear,'_LbMv','.png'), plot = g.map.TaSWin, path = fpath, width = 12, height = 10)
 
 
 
