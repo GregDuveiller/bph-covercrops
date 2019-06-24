0ae1eaae72ca054c085bc037633c9f7f37bc681e
duveigr <gregory.duveiller@ec.europa.eu>
2019-06-24 16:54:24 +0000

    Word on figures

--- codeFigures/fig__use-of-RS.r
+++ codeFigures/fig__use-of-RS.r
@@ -8,9 +8,13 @@ library(RStoolbox)
 library(here)
 
 pt_ID <- '28761622'
+pt_ID <- '34362088'
 
-
+laes_prj <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
 buf <- sf::st_read('dataFigures/dat4figLUCASpt/arable_buffer.shp', quiet = TRUE)
+bufzone <- buf %>% filter(POINT_ID == pt_ID)
+pts <- sf::st_read('dataFigures/dat4figLUCASpt/arable.shp', quiet = TRUE)
+ptszone <- pts %>% filter(POINT_ID == pt_ID)
 
 
 L8_Winter = stack(paste0('dataFigures/dat4figLUCASpt/pt_',pt_ID,'/L8_clip_winter_',pt_ID,'.tif'))
@@ -19,70 +23,87 @@ L8_Summer = stack(paste0('dataFigures/dat4figLUCASpt/pt_',pt_ID,'/L8_clip_summer
 
 Albedo <- stack(list(winter = raster(paste0('dataFigures/dat4figLUCASpt/pt_',pt_ID,'/albedo_winter_',pt_ID,'.tif')),
                      summer = raster(paste0('dataFigures/dat4figLUCASpt/pt_',pt_ID,'/albedo_summer_',pt_ID,'.tif'))))
-Albedo_df <- as.data.frame(0.0001 * Albedo, xy = T, long = T)
+Albedo_df <- as.data.frame(0.001 * Albedo, xy = T, long = T)
 colnames(Albedo_df) <- c('Easting','Northing','Season','Albedo')
 
 
-laes_prj <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
-bufzone <- buf %>% filter(POINT_ID == pt_ID) %>%
-  sf::st_transform(laes_prj)
 
 
-xLims <- c(-6.15,-6.05)
-yLims <- c(36.25, 36.35)
+# plot parametrizations
+ALB_lims <- c(0,0.2)
+
+
 
 # RGB plots
 g.map.L8.winter <- ggRGB(img = L8_Winter, r = 3, g = 2, b = 1,
                       stretch = 'hist') + 
-  geom_sf(data = bufzone, color = 'red') +
+  geom_sf(data = bufzone, color = 'red', fill = "transparent") +
   scale_y_continuous('Northing') + 
-  scale_x_continuous('Easting') +
-  coord_sf(xlim = xLims, ylim = yLims)
+  scale_x_continuous('Easting') + 
+  coord_sf(expand = F)
 
 g.map.L8.summer <- ggRGB(img = L8_Summer, r = 3, g = 2, b = 1,
                       stretch = 'hist') + 
+  geom_sf(data = bufzone, color = 'red', fill = "transparent") +
   scale_y_continuous('Northing') + 
-  scale_x_continuous('Easting') +
-  coord_cartesian(xlim = xLims, ylim = yLims)
+  scale_x_continuous('Easting') + 
+  coord_sf(expand = F)
 
 
 # Albedo maps 
 g.map.Albedo.winter <- ggplot(Albedo_df %>% filter(Season == 'winter')) +
   geom_raster(aes(x = Easting, y = Northing, fill = Albedo)) +
   geom_sf(data = bufzone, color = 'red', fill = "transparent") +
-  scale_fill_viridis_c(limits=c(0,0.02), oob=squish, na.value = "transparent") + ## option = "inferno"  +
-  #coord_sf(xlim = xLims, ylim = yLims) +
-  theme(legend.position = c(0.1,0.1))
+  geom_sf(data = ptszone, color = 'red') +
+  scale_fill_viridis_c(limits = ALB_lims, oob = squish, na.value = "transparent") + ## option = "inferno"  +
+  coord_sf(expand = F) +
+  theme(legend.position = c(0.1,0.1),
+        axis.title = element_blank())
 
 g.map.Albedo.summer <- ggplot(Albedo_df %>% filter(Season == 'summer')) +
   geom_raster(aes(x = Easting, y = Northing, fill = Albedo)) +
-  scale_fill_viridis_c(limits=c(0,0.02), oob=squish, na.value = "transparent") + ## option = "inferno"  +
-  coord_cartesian(xlim = xLims, ylim = yLims) +
+  geom_sf(data = bufzone, color = 'red', fill = "transparent") +
+  scale_fill_viridis_c(limits = ALB_lims, oob = squish, na.value = "transparent") + ## option = "inferno"  +
+  coord_sf(expand = F) +
+  theme(legend.position = c(0.1,0.1),
+        axis.title = element_blank())
+
+
+
+### ... 
+
+
+
+g.map.albedo <- ggplot(filter(dat, time == '2016-10-07'))+
+  geom_raster(aes(x = col, y = row, fill = ALB))+
+  geom_point(data = filter(dat, idpt == 221, time == '2016-10-07'),
+             aes(x = col, y = row), size = 5, shape = 3, colour = 'cyan')+
+  geom_point(data = filter(dat,time == '2008-10-07', SNR > minSNR, dist < minDist),
+             aes(x = col, y = row), shape = 21, fill = 'grey80', colour = 'grey50', size = 4)+
+  scale_fill_viridis_c(limits = ALB_lims, oob = squish, na.value = "transparent")+
+  coord_fixed()+
   theme(legend.position = c(0.1,0.1))
 
 
+# SNR
+g.map.snr <- ggplot(filter(dat, time == '2016-10-07'))+
+  geom_raster(aes(x = col, y = row, fill = SNR))+
+  geom_point(data = filter(dat, idpt == 221, time == '2016-10-07'),
+             aes(x = col, y = row), size = 5, shape = 3, colour = 'cyan')+
+  geom_point(data = filter(dat,time == '2008-10-07', SNR > minSNR, dist < minDist),
+             aes(x = col, y = row), shape = 21, fill = 'grey80', colour = 'grey50', size = 4)+
+  scale_fill_viridis_c(option='C', limits = c(0,70), oob = squish)+
+  coord_fixed()+
+  theme(legend.position = c(0.1,0.1))
 
 
-ggplot(Albedo_df) +
-  geom_raster(aes(x, y, fill = albedo)) +
-  scale_fill_viridis_c(limits=c(0,200), oob=squish, na.value = "transparent") + ## option = "inferno"  +
-  # coord_sf(xlim=c(2.5e6,6e6),ylim=c(1.5e6,5.4e6))+
-  xlab("") + ylab("")+
-  facet_wrap(~season)+  theme_bw()+
-  labs(fill = expression(atop(
-    "Albedo" )))+
-  # theme_Publication()+
-  theme(panel.grid.minor=element_blank(),
-        strip.text = element_text(size = 20),
-        legend.key.width = unit(1.5,'cm'),
-        plot.title = element_text(size = 20, face = "bold"),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks.y=element_blank(),
-        legend.position = "bottom",
-        legend.direction = "horizontal",
-        axis.title.x=element_blank(),axis.title.y=element_blank(),
-        legend.title=element_text(size=20), 
-        legend.text=element_text(size=20)
-  )
+# regression
+g.plot.ALBvsNDVI <- ggplot(filter(datf0, time <= timeMax, time > timeMin),
+                      aes(x = NDV, y = ALB)) + 
+  geom_point(aes(x=NDV,y=ALB,fill=as.integer(time)), shape=21, colour='grey40',size=2)+
+  geom_smooth(method = 'lm', colour=pal[2],fullrange=T)+
+  geom_point(data=filter(datf0,dist==0,time<=timeMax,time>timeMax-ndays),
+             aes(fill=as.integer(time)), shape=21, colour='grey20',size=5)+
+  scale_fill_gradientn('Time', colours = pal, labels = as.Date_origin, limits = TIME_lims)+
+  coord_cartesian(ylim = ALB_lims)+
+  theme(legend.position = 'none') #c(0.8,0.2)
